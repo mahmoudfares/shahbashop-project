ea66e9d513c68f2cca1ef2a6a3af999ccf320fc2
Mahmoud <mahmut.faris1993@gmail.com>
2020-03-03 22:02:00 +0100

    Added form validation

--- shahbashop-UI/src/pages/signIn/SignIn.js
+++ shahbashop-UI/src/pages/signIn/SignIn.js
@@ -1,50 +1,28 @@
-import React, {Component} from "react";
-import { useForm } from "react-hook-form";
+import React from "react";
 import { LoginService as SignInService } from "../../services/AuthService";
 import { setToken } from "../../utils/cookies";
 import { useTranslation } from "react-i18next";
 import "./signIn.scss";
 import { NavLink } from "react-router-dom";
-import { FaEnvelope,FaKey } from "react-icons/fa";
+import { FaEnvelope, FaKey } from "react-icons/fa";
+import { withFormik, Form, Field } from "formik";
+const Yup = require('yup')
 
-const SignIn = (props) => {
+
+const SignInForm = props => {
+  const { values, touched, errors, handleChange, handleSubmit, isSubmitting, isValidating } = props;
+
   const { t } = useTranslation();
-  
-  const { register, handleSubmit, errors } = useForm();
 
-  const formErrors = {
-    email: {
-      message: ""
-    }
-  };
+  let emailError = touched.email && errors.email && t("shared.email") + " " + t(`errors.${errors.email}`);
+  let passwordError = touched.password && errors.password && t("shared.password") + " " + t(`errors.${errors.password}`);
 
-  const validateForm = () => {
+  console.log(props.isValid)
 
-    console.log(errors);
 
-    if (errors.email) {
-      errors.email.type === "required"
-        ? (formErrors.email.message = "Email field is required")
-        : (formErrors.email.message = "Email field min length is 2");
-    }
-  };
-
-  const onSubmit = (data) => {
-    SignInService(data.email, data.password)
-      .then(res => {
-        setToken(res.data.token);
-        props.history.goBack();
-      })
-      .catch(err => console.log(err));
-  }
-
-  validateForm();
-
-console.log(errors)
   return (
-    
-    <div className="col-lg-12 bg-warning login">
-      <form onSubmit={handleSubmit(onSubmit)} className="col-lg-10 bg-white login-form">
+    <div className="col-lg-12 bg-warning login row">
+      <Form className="col-lg-10 bg-white login-form align-self-center">
         <img className="logo" alt="logo" src={require(`../../images/White-square.jpg`)} />
         
         <h1 className="text-center title">{t("signin.name")}</h1>
@@ -52,31 +30,54 @@ console.log(errors)
 
         <div className="input-group mb-3">
           <div className="input-group-prepend">
-            <span className="input-group-text  bg-warning text-dark" id="basic-addon1"><FaEnvelope /></span>
+            <span className={`input-group-text  bg-warning ${emailError ? "text-danger" : "text-dark"}`} id="basic-addon1"><FaEnvelope /></span>
           </div>
-          <input placeholder={t("shared.enterEmail")} className="form-control" type="email" name="email" ref={register({ required: true, minLength: 2 })} />
+          <Field placeholder={t("shared.enterEmail")} className="form-control" type="email" name="email" onChange={handleChange} value={values.email}/>
           <div className="input-border" id="email-input-border"/>
         </div>
         
+        {emailError ? <div className="alert alert-danger mb-2">{emailError}</div> : <div className="mb-3" />}
+
         <div className="input-group mb-3">
           <div className="input-group-prepend">
-              <span className="input-group-text  bg-warning text-dark" id="basic-addon1"><FaKey /></span>
+              <span className={`input-group-text  bg-warning ${passwordError ? "text-danger" : "text-dark"}`} id="basic-addon1"><FaKey /></span>
           </div>
-          <input className="form-control" name="password" type="password" name="password" ref={register} placeholder={t("shared.enterPassword")} ref={register({require: true})} />
-          {errors.email && <p>email is require</p>}
-
+          <Field className="form-control" type="password" name="password" placeholder={t("shared.enterPassword")} onChange={handleChange} value={values.password}/>
           <div className="input-border" id="password-input-border"/>
         </div>
+        {passwordError ? <div className="alert alert-danger mb-2">{passwordError}</div> : <div className="mb-3" />}
+
         <div>
            <NavLink className="note" to="/signup">{t("signin.notCustomerYet")}</NavLink>
         </div>
         <div className="text-center">
-          <input className="btn btn-warning" type="submit" value={t("signin.name")} />
+          <input disabled={isSubmitting} className="btn btn-warning" type="submit" value={t("signin.name")} />
         </div>
-        </form>
+        </Form>
     </div>
       
   );
 }
+
+const SignIn = withFormik({
+  mapPropsToValues(props) {
+    return {
+      email: "",
+      password: ""
+    }
+  },
+  validationSchema: Yup.object().shape({
+    email: Yup.string().email("notValid").required("required"),
+    password: Yup.string().min(8, "tooShort").required("required").matches(/^(?=.*[a-z])(?=.*[0-9])(?=.{8,})/,'passwordAllowedCharacters')
+  }),
+  handleSubmit(values) {
+    SignInService(values.email, values.password)
+      .then(res => console.log(res))
+      .catch(err => console.log(err))
+  },
+  handleChange(value) {
+    console.log(value)
+  }
+})(SignInForm);
 
 export default SignIn;
--- shahbashop-UI/src/pages/signUp/SignUp.js
+++ shahbashop-UI/src/pages/signUp/SignUp.js
@@ -1,88 +1,85 @@
-import React, {useState} from "react";
+import React from "react";
 import "./signUp.scss";
 import { useTranslation } from "react-i18next";
 import { NavLink } from "react-router-dom";
 import { FaEnvelope,FaKey,FaCheck } from "react-icons/fa";
+import { withFormik, Form, Field } from "formik";
+const Yup = require('yup');
 
 
-
-
-export default function SignUp() {
+const SignUpForm = props => {
     const { t } = useTranslation();
+    const { touched, errors, handleChange, handleSubmit, isSubmitting } = props;
 
-    const [emailErrors, setEmailErrors] = useState("");
-    const [passwordErrors, setPasswordErrors] = useState("");
-    const [passwordConfirmErrors, setPasswordConfirmErrors] = useState("");
-
-    
-    const onSubmit = (event) => {
-        console.log(event)
-        event.preventDefault();
-    }
-    
-    const onChange = (e) => {
-        let { name, value } = e.target;
-        if (name = "email") {
-            validateEmail(value);
-            return;
-        }
-    }
-
-    const validateEmail = (email) => {
-        if (email === "") setEmailErrors("email is required");
-        else if (email.length < 5) setEmailErrors("email must be larger then 5 characters")
-        else setEmailErrors("");
-    }
-
-    const validatePassword = (password) => {
-        if (password === "") setEmailErrors("email is required");
-        else if (password.length < 5) setEmailErrors("email must be larger then 5 characters")
-        else setEmailErrors("");
-    }
-
-    const validatePasswordConfirm = (confirmPassword) => {
-        if (confirmPassword === "") setEmailErrors("email is required");
-        else if (confirmPassword.length < 5) setEmailErrors("email must be larger then 5 characters")
-        else setEmailErrors("");
-    }
+    let emailError = touched.email && errors.email && t("shared.email") + " " + t(`errors.${errors.email}`);
+    let passwordError = touched.password && errors.password && t("shared.password") + " " + t(`errors.${errors.password}`);
+    let passwordConfirmationError = touched.passwordConfirmation && errors.passwordConfirmation && t(`shared.passwordConfirmation`) + " " + t(`errors.${errors.passwordConfirmation}`);
 
     return (
-        <div className="col-lg-12 register bg-warning">
-            <form className="col-lg-10 register-form bg-white" onSubmit={onSubmit}>
+        <div className="col-lg-12 register bg-warning row">
+            <Form className="col-lg-10 register-form bg-white align-self-center">
               <img className="logo" alt="logo" src={require(`../../images/White-square.jpg`)} />
                 <h1 className="text-center title">{t("signup.name")}</h1>
                 <p>{t("signup.welcomeText")}</p>
                 <div className="input-group mb-3">
                     <div className="input-group-prepend">
-                        <span className="input-group-text  bg-warning text-dark" id="basic-addon1"><FaEnvelope /></span>
+                        <span className={`input-group-text  bg-warning ${emailError ? "text-danger" : "text-dark"}`} id="basic-addon1"><FaEnvelope /></span>
                     </div>
-                    <input placeholder={t("shared.enterEmail")} className="form-control" id="inputError" type="email" name="email" onChange={onChange} required/>
+                    <Field placeholder={t("shared.enterEmail")} className="form-control" id="inputError" type="email" name="email" onChange={handleChange} />
                     <div className="input-border" id="email-input-border" />
                 </div>
-                {emailErrors ? <div className="alert alert-danger mb-2">{emailErrors}</div> : <div className="mb-3" />}
+                {emailError ? <div className="alert alert-danger mb-2">{emailError}</div> : <div className="mb-3" />}
 
                 <div className="input-group mb-4">
                     <div className="input-group-prepend">
-                        <span className="input-group-text  bg-warning text-dark" id="basic-addon1"><FaKey /></span>
+                        <span className={`input-group-text  bg-warning ${passwordError ? "text-danger" : "text-dark"}`} id="basic-addon1"><FaKey /></span>
                     </div>
-                    <input placeholder={t("shared.enterPassword")} className="form-control" type="password" name="password" onChange={onChange} required/>
+                    <Field placeholder={t("shared.enterPassword")} className="form-control" type="password" name="password" onChange={handleChange} required/>
                     <div className="input-border"/>
                 </div>
+                {passwordError ? <div className="alert alert-danger mb-2">{passwordError}</div> : <div className="mb-3" />}
+
                 <div className="input-group mb-4">
                 <div className="input-group-prepend">
-                        <span className="input-group-text bg-warning text-dark" id="basic-addon1"><FaCheck /></span>
+                        <span className={`input-group-text  bg-warning ${passwordConfirmationError ? "text-danger" : "text-dark"}`} id="basic-addon1"><FaCheck /></span>
                     </div>
-                    <input placeholder={t("shared.reWritePassword")} className="form-control" type="password" name="password" onChange={onChange} required/>
+                    <Field placeholder={t("shared.reWritePassword")} className="form-control" type="password" name="passwordConfirmation" onChange={handleChange} required/>
                     <div className="input-border"/>
                 </div>
+                {passwordConfirmationError ? <div className="alert alert-danger mb-2">{passwordConfirmationError}</div> : <div className="mb-3" />}
+
                 <div>
                     <NavLink className="note" to="/signin">{t("signup.alreadyCustomer")}</NavLink>
                 </div>
                 <div className="text-center">
                     <input className="btn btn-warning" type="submit" value={t("signup.name")} />
                 </div>
-            </form>
+            </Form>
         </div>
     );
 };
 
+const SignUP = withFormik({
+    mapPropsToValues(props) {
+      return {
+        email: "",
+        password: "",
+        passwordConfirmation: ""
+      }
+    },
+    validationSchema: Yup.object().shape({
+      email: Yup.string().email("notValid").required("required"),
+      password: Yup.string().min(8, "tooShort").required("required").matches(/^(?=.*[a-z])(?=.*[0-9])(?=.{8,})/,'passwordAllowedCharacters'),
+      passwordConfirmation: Yup.string().oneOf([Yup.ref('password'), null], 'passwordNotMatch').required("required")
+    }),
+    handleSubmit(values) {
+    //   SignInService(values.email, values.password)
+    //     .then(res => console.log(res))
+    //     .catch(err => console.log(err))
+    },
+    handleChange(value) {
+      console.log(value)
+    }
+})(SignUpForm);
+  
+export default SignUP;
